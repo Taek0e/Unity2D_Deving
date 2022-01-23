5447fb343471ecdd677ab3203676705e4cc9310e
Taek0e <bnskim05@gmail.com>
2022-01-24 01:54:01 +0900

    2022.01.24 RPG 게임 개발

---
+++ PracticeRPG/Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
