d96d21fd0c99892568e4558f0feab3ff629e39cf
Taek0e <bnskim05@gmail.com>
2022-02-09 00:57:24 +0900

    2022.02.09 퀘스트중 Task 부문 좀만 더 공부하면 이해할듯..

--- QuestDevPractice/Assets - 복사본/Scripts/Quest/Task/Task.cs
+++ QuestDevPractice/Assets - 복사본/Scripts/Quest/Task/Task.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using UnityEngine;
 
 
-[CreateAssetMenu(menuName = "Quest/Task/Task", fileName = "Task_")]
+public enum TaskState
+{
+    Inactive,
+    Running,
+    Complete
+}
 
+
+[CreateAssetMenu(menuName = "Quest/Task/Task", fileName = "Task_")]
 public class Task : ScriptableObject
 {
+    #region Events
+    public delegate void StateChangedHandler(Task task, TaskState curentState, TaskState prevState);
+    public delegate void SuccessChangedHandler(Task task, int currentSuccess, int prevSuccess);
+    #endregion 
+
+
+    [SerializeField]
+    private Category category;
+
     [Header("Text")]
     [SerializeField]
-    private string codeName; 
+    private string codeName;
     [SerializeField]
     private string description;
 
@@ -28,12 +44,65 @@ public class Task : ScriptableObject
     [SerializeField]
     private int needSuccessToComplete;
 
-    public int CurrentSuccess { get; private set; }
+
+    private TaskState state;
+
+    public int currentSuccess;
+
+    public event StateChangedHandler onStateChanged;
+    public event SuccessChangedHandler onSuccessChanged;
+
+    public int CurrentSuccess
+    {
+        get => currentSuccess;
+        set
+        {
+            int prevSuccess = currentSuccess;
+            currentSuccess = Mathf.Clamp(value, 0, needSuccessToComplete);
+
+            if (currentSuccess != prevSuccess)
+            {
+                State = currentSuccess == needSuccessToComplete ? TaskState.Complete : TaskState.Running;
+                onSuccessChanged?.Invoke(this, currentSuccess, prevSuccess);
+            }
+        }
+    }
+
+    public Category Category => category;
     public string CodeName => codeName;
     public string Description => description;
     public int NeedSuccessToComplete => needSuccessToComplete;
 
+    public TaskState State
+    {
+        get => state;
+        set
+        {
+            var prevState = state;
+            state = value;
+            onStateChanged?.Invoke(this, state, prevState);  // onStateChanged가 null이 아니라면 Invoke() 실행
+        }
+    }
 
+    public bool IsComplete => State == TaskState.Complete;
+
+    public Quest Owner { get; private set; }
+
+    public void Setup(Quest quest)
+    {
+        Owner = quest;
+    }
+
+    public void Start()
+    {
+        State = TaskState.Running; 
+    }
+
+    public void End()
+    {
+        onStateChanged = null;
+        onSuccessChanged = null;
+    }
 
     public void ReceieveReport(int successCount)
     {
@@ -41,10 +110,14 @@ public class Task : ScriptableObject
         
     }
 
-    public bool IsTarget(object target) => targets.Any(x => x.IsEqual(target));
+    public bool IsTarget(string category, object target)
+        => Category == category ||
+        targets.Any(x => x.IsEqual(target)) &&
+        !IsComplete; 
 
-        
-     
+
+
+
 
 
 }
